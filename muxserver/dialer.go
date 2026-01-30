9629790d902430c2db3fcf9c74c29afdb817b539
WangZY <76583642+xmx@users.noreply.github.com>
2026-01-30 17:57:43 +0800

    Enhance `NewMixedDialer` with additional parameters and improve dialing logic

--- muxserver/dialer.go
+++ muxserver/dialer.go
@@ -2,17 +2,25 @@ package muxserver
 
 import (
 	"context"
+	"errors"
 	"net"
+	"strings"
 
 	"github.com/vela-ssoc/ssoc-proto/muxproto"
 )
 
-func NewMixedDialer(mux muxproto.MUXOpener) muxproto.Dialer {
-	return &mixedDialer{mux: mux}
+func NewMixedDialer(mux muxproto.MUXOpener, hub Huber, sys muxproto.Dialer) muxproto.Dialer {
+	return &mixedDialer{
+		mux: mux,
+		hub: hub,
+		sys: sys,
+	}
 }
 
 type mixedDialer struct {
 	mux muxproto.MUXOpener
+	hub Huber
+	sys muxproto.Dialer
 }
 
 func (m *mixedDialer) DialContext(ctx context.Context, network, address string) (net.Conn, error) {
@@ -21,9 +29,37 @@ func (m *mixedDialer) DialContext(ctx context.Context, network, address string) 
 		return nil, err
 	}
 
-	if host == m.mux.Host() {
+	if m.mux != nil && host == m.mux.Host() {
 		return m.mux.Open(ctx)
 	}
 
-	return nil, net.UnknownNetworkError("未知的主机")
+	if m.hub != nil {
+		_, domain, found := strings.Cut(host, ".")
+		if found && domain == m.hub.Domain() {
+			peer := m.hub.Get(host)
+			if peer == nil {
+				return nil, &net.OpError{
+					Op:   "dial",
+					Net:  network,
+					Addr: &net.UnixAddr{Net: network, Name: address},
+					Err:  errors.New("节点不存在或已经离线"),
+				}
+			}
+
+			mux := peer.MUX()
+
+			return mux.Open(ctx)
+		}
+	}
+
+	if m.sys != nil {
+		return m.sys.DialContext(ctx, network, address)
+	}
+
+	return nil, &net.OpError{
+		Op:     "dial",
+		Net:    address,
+		Source: &net.UnixAddr{Net: network, Name: address},
+		Err:    errors.New("没有找到合适的拨号器"),
+	}
 }
