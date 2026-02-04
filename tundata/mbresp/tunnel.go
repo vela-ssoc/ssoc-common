fd82a4c04af0cc4f399d0fa98c5177ecb246ac72
WangZY <76583642+xmx@users.noreply.github.com>
2026-02-04 13:38:18 +0800

    Add `Name`, `Module`, `Cumulative`, and `Active` fields to `TunnelStat` struct

--- tundata/mbresp/tunnel.go
+++ tundata/mbresp/tunnel.go
@@ -1,8 +1,12 @@
 package mbresp
 
 type TunnelStat struct {
-	RX      uint64  `json:"rx"`
-	TX      uint64  `json:"tx"`
-	Limit   float64 `json:"limit"`
-	Unlimit bool    `json:"unlimit"`
+	Name       string  `json:"name"`
+	Module     string  `json:"module"`
+	Cumulative int64   `json:"cumulative"`
+	Active     int64   `json:"active"`
+	RX         uint64  `json:"rx"`
+	TX         uint64  `json:"tx"`
+	Limit      float64 `json:"limit"`
+	Unlimit    bool    `json:"unlimit"`
 }
