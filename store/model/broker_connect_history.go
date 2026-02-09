aec004432c9cc19955bee2a8f5dcf6f783d42c81
WangZY <76583642+xmx@users.noreply.github.com>
2026-02-09 13:55:03 +0800

    Update BrokerConnectHistory field and Go version
    Rename `Broker` to `BrokerID` in the `BrokerConnectHistory` struct for clarity and update the Go version to 1.26.0-rc.3 in the GitHub Actions workflow.

--- store/model/broker_connect_history.go
+++ store/model/broker_connect_history.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/v2/bson"
 
 type BrokerConnectHistory struct {
 	ID         bson.ObjectID     `bson:"_id,omitempty"    json:"id"`
-	Broker     bson.ObjectID     `bson:"broker_id"        json:"broker_id"`
+	BrokerID   bson.ObjectID     `bson:"broker_id"        json:"broker_id"`
 	Name       string            `bson:"name"             json:"name"`
 	Semver     string            `bson:"semver,omitempty" json:"semver,omitzero"`
 	Inet       string            `bson:"inet,omitempty"   json:"inet,omitzero"`
