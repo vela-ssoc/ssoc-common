d55bf380f3f88cb36b5bb9acefdd3bc0f87a3158
WangZY <76583642+xmx@users.noreply.github.com>
2026-02-26 18:07:14 +0800

    参数校验

--- validation/custom.go
+++ validation/custom.go
@@ -8,13 +8,23 @@ import (
 func All() []CustomValidatorFunc {
 	return []CustomValidatorFunc{
 		uniqueFunc,
+		mongodbFunc,
 	}
 }
 
 func uniqueFunc() (string, validator.FuncCtx, validator.RegisterTranslationsFunc) {
 	const tag = "unique"
 	trans := func(utt ut.Translator) error {
-		return utt.Add(tag, "{0}内数据不能重复", true)
+		return utt.Add(tag, "{0} 内数据不能重复", true)
+	}
+
+	return tag, nil, trans
+}
+
+func mongodbFunc() (string, validator.FuncCtx, validator.RegisterTranslationsFunc) {
+	const tag = "mongodb"
+	trans := func(utt ut.Translator) error {
+		return utt.Add(tag, "{0} 格式错误", true)
 	}
 
 	return tag, nil, trans
