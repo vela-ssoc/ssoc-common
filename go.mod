f349197a8c649678f4ba49c4a64505883287b4f8
WangZY <76583642+xmx@users.noreply.github.com>
2026-03-03 14:54:16 +0800

    增强 muxconn

--- go.mod
+++ go.mod
@@ -10,7 +10,7 @@ require (
 	github.com/lmittmann/tint v1.1.3
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/shirou/gopsutil/v4 v4.26.2
-	github.com/vela-ssoc/ssoc-proto v0.0.0-20260302055533-a55ad4e4b97e
+	github.com/vela-ssoc/ssoc-proto v0.0.0-20260303064715-c16232b12437
 	github.com/xgfone/ship/v5 v5.3.2
 	github.com/xtaci/smux v1.5.56
 	go.mongodb.org/mongo-driver/v2 v2.5.0
--- tundata/mbresp/tunnel.go
+++ tundata/mbresp/tunnel.go
@@ -1,12 +1,17 @@
 package mbresp
 
+import (
+	"github.com/vela-ssoc/ssoc-proto/muxconn"
+)
+
 type TunnelStat struct {
-	Name       string  `json:"name"`
-	Module     string  `json:"module"`
-	Cumulative int64   `json:"cumulative"`
-	Active     int64   `json:"active"`
-	RX         uint64  `json:"rx"`
-	TX         uint64  `json:"tx"`
-	Limit      float64 `json:"limit"`
-	Unlimit    bool    `json:"unlimit"`
+	Name       string                 `json:"name"`
+	Module     string                 `json:"module"`
+	Cumulative int64                  `json:"cumulative"`
+	Active     int64                  `json:"active"`
+	RX         uint64                 `json:"rx"`
+	TX         uint64                 `json:"tx"`
+	Limit      float64                `json:"limit"`
+	Unlimit    bool                   `json:"unlimit"`
+	Streams    []*muxconn.StreamStats `json:"streams"`
 }
